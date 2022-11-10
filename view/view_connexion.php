10022f723aa259041c6f01c1912e6e864299f4e4
waniemo <evaducere@gmail.com>
2022-11-10 16:37:29 +0100

    Modification view connexion

---
+++ view/view_connexion.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Connexion :</title>
+</head>
+<body>
+    <h1>Connexion :</h1>
+    <form action=" " method="post">
+        <p>Veuillez saisir votre identifiant :</p>
+        <input type="text" name="login">
+        <p>Veuillez saisir votre mot de passe :</p>
+        <input type="text" name="mdp">
+        <input type="submit" value="connexion">
+    </form>
+</body>
+</html>
